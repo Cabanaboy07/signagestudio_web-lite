2c88695f969da9b1f6a8eb84c307c09403583959
Cabanaboy07 <Cabanaboy07@aol.com>
2016-07-16 10:52:16 -0500

    443 Update
    Reference: http://script.digitalsignage.com/forum/index.php/topic,6791.msg18968.html#msg18968

--- StudioLite.js
+++ StudioLite.js
@@ -42,7 +42,7 @@ define(['underscore', 'jquery', 'backbone', 'bootstrap', 'backbone.controller', 
             BB.platform = platform;
             BB.lib.forceBrowserCompatibility();
             BB.lib.promptOnExit();
-            BB.CONSTS.ROOT_URL = 'https://secure.digitalsignage.com' + (BB.lib.inDevMode() ? ':442' : '');
+            BB.CONSTS.ROOT_URL = 'https://secure.digitalsignage.com' + (BB.lib.inDevMode() ? ':443' : '443');
             BB.CONSTS.BASE_URL = BB.CONSTS.ROOT_URL + (BB.lib.inDevMode() ? '/_studiolite-dev/studiolite.html' : '/_studiolite-dist/studiolite.html');
 
             //console.log(platform.name + ' ' + platform.version);
